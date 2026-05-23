9ec78b341a424b2007ad621e2133bedc7b88a3b0
Saberblaze22 <Comiccooldylan@gmail.com>
2026-05-22 18:22:16 -0600

    change sql query to join organization for organization name

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -2,8 +2,9 @@ import db from './db.js'
 
 const getAllProjects = async() => {
     const query = `
-        SELECT Project_id, title, description, location, project_date, organization
-      FROM public.Project;
+        SELECT title, project_date, name
+      FROM public.Project p
+      JOIN public.Organization o ON p.organization_id = o.organization_id;
     `;
 
     const result = await db.query(query);
